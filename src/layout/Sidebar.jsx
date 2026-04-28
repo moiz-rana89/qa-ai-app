00eec8fb41072f2353cf5f1180fe324533682543
abdulmoizrana <abdulmoizrana.ar@gmail.com>
2026-04-29 00:24:17 +0500

    feat: added gorgious form

--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -111,11 +111,11 @@ const menuList = [
         route: "wfa-ontime-reporting",
         roles: ["dev", "wfa"],
       },
-      {
-        title: "Attendance Automation Infractions",
-        route: "attendance-infractions",
-        roles: ["admin", "dev", "wfa"],
-      },
+      // {
+      //   title: "Attendance Automation Infractions",
+      //   route: "attendance-infractions",
+      //   roles: ["admin", "dev", "wfa"],
+      // },
     ],
   },
   {
@@ -270,12 +270,12 @@ const menuList = [
   //   route: "performance-review",
   //   roles: ["admin", "dev", "tl"],
   // },
-  {
-    title: "Bugs & Features",
-    icon: "mdi:bug-outline",
-    route: "bugs-features",
-    roles: ["admin", "dev"],
-  },
+  // {
+  //   title: "Bugs & Features",
+  //   icon: "mdi:bug-outline",
+  //   route: "bugs-features",
+  //   roles: ["admin", "dev"],
+  // },
   {
     title: "Quality Assurance",
     icon: "icon-park-outline:success",
